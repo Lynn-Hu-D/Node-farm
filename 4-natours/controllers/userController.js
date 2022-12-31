011ecea746540db4d9542169a3326f58d18ca1fc
Lynn-Hu-D <lingyuhu0106@gmail.com>
2022-12-31 02:13:07 -0800

    middlewares and morgan

---
+++ 4-natours/controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This route is not test define!',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This route is not test define!',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This route is not test define!',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This route is not test define!',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This route is not test define!',
+  });
+};
