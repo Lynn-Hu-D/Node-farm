6db337cfc2b1da2d09f287eee463551ad967ea79
Lynn-Hu-D <lingyuhu0106@gmail.com>
2023-01-28 11:56:38 -0800

    error handling

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorConstroller');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -12,23 +14,24 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-//app.use(express.json());
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleWare');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(x);
   next();
 });
 
 // 3) Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 // 4)start server
 module.exports = app;
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION!💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -18,11 +24,24 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true
   })
-  .then(() => {
-    console.log('DB connection successful!');
-  });
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLER REJECTION!💥 Shutting down...');
+  //console.log(err.name, err.message);
+  const fullMessage = err.message;
+  const errmsgStart = fullMessage.indexOf('errmsg:') + 97; // Start at the beginnning
+  const newline = /\n/; // new line character
+  const errmsgStop = fullMessage.search(newline); // Find new line
+  const errmsgLen = errmsgStop - errmsgStart;
+  const errorText = fullMessage.substr(errmsgStart, errmsgLen);
+  console.log(errorText);
+  server.close(() => {
+    process.exit(1);
+  });
 });
